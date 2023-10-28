5db7805852a084fe8310e1be0bbd89c640b6be00
jo.yunji <ec2-user@ip-172-31-18-249.ap-northeast-1.compute.internal>
2023-10-28 10:03:02 +0000

    add/管理者#会員一覧

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,4 +1,7 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_customer!
+  before_action :set_current_cusotmer
+  
   def show
   end
 
@@ -6,5 +9,26 @@ class Public::CustomersController < ApplicationController
   end
 
   def unsubscribe
+    if @customer.update(customer_params)
+      redirect_to mypage_path, notice: '会員情報の更新が完了しました。'
+    else
+      render :edit
+    end
+  end
+  
+  def withdraw
+    @customer.update(is_active: false)
+    reset_session
+    redirect_to root_path
+  end
+  
+  private
+  
+  def set_current_customer
+    @customer = current_customer
+  end
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :post_code, :address, :phone_number)
   end
 end
