9d945c28927a066e6d2d4d9566163609af492d06
jo.yunji <ec2-user@ip-172-31-18-249.ap-northeast-1.compute.internal>
2023-11-02 06:38:53 +0000

    fixing now/cart_item

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,8 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :cart_items
 
   validates :last_name, presence: true
   validates :first_name, presence: true
