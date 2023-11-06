221e8837bcc50aa99be01c6bd870c8ef07472cf2
jo.yunji <ec2-user@ip-172-31-18-249.ap-northeast-1.compute.internal>
2023-11-06 06:38:44 +0000

    add/edit update機能

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -7,13 +7,28 @@ class Public::AddressesController < ApplicationController
     @address = Address.new
   end
 
+  def create
+    @addresses = current_customer.addresses
+    # @address = @addresses.new(address_params)
+    # こう書くと、save出来なかった時に、@addressesの最後に空レコードが入り、エラーになる。
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      redirect_to addresses_path
+    else 
+      render :index
+    end
+  end
+  
   def edit
   end
   
-  def create
-  end
-  
   def update
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render :edit
+    end
   end
   
   def destroy
@@ -26,7 +41,7 @@ class Public::AddressesController < ApplicationController
   end
   
   def ensure_address
-    @addresses = current_customer.address
+    @addresses = current_customer.addresses
     @address = @addresses.find_by(id: params[:id])
     redirect_to addresses_path unless @address
   end
