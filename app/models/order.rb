372c7fa798cb5e8ddfd71701457169814b2d3c97
jo.yunji <ec2-user@ip-172-31-18-249.ap-northeast-1.compute.internal>
2023-11-06 13:33:12 +0000

    enum導入

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,4 +9,9 @@ class Order < ApplicationRecord
   validates :shipping_cost, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
   validates :total_payment, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
   
+  
+  enum payment_method: { credit_card: 0, transfer: 1 }
+  enum status: { waiting_deposit: 0, confirm_deposit: 1, in_production: 2, preparing_shipment: 3, shipped: 4 }
+  
+  
 end
