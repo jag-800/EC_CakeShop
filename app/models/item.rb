54fe01539513dc3ef2b67b8091a694385b69d6ab
jo.yunji <ec2-user@ip-172-31-18-249.ap-northeast-1.compute.internal>
2024-01-22 09:47:49 +0000

    画像追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,8 +11,8 @@ class Item < ApplicationRecord
 
   def get_item_image(width, height)
     unless item_image.attached?
-      file_path = Rails.root.join('app/assets/images/default-image.jpg')
-      item_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      file_path = Rails.root.join('app/assets/images/shortcake.png')
+      item_image.attach(io: File.open(file_path), filename: 'shortcake.png', content_type: 'image/jpeg')
     end
     # item_image.variant(resize_to_limit: [width, height]).processed
     #上のコードだと元ん画像のサイズより大きくできない
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,60 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.create!(email: 'admin@example.com', password: 'password')
+Admin.create!(email: 'a@a', password: 'aaaaaa')
 
 Customer.create!(
-  password: 'aaaaaa',
-  last_name: '令和',
-  first_name: '道子',
-  last_name_kana: 'レイワ',
-  first_name_kana: 'ミチコ',
-  email: "sample@example.com",
-  post_code: '0000000',
-  address: '東京都渋谷区神園町0-0',
-  phone_number: '0000000000',
-  is_active: true
+  [
+    {
+      password: '000000',
+      last_name: '令和',
+      first_name: '道子',
+      last_name_kana: 'レイワ',
+      first_name_kana: 'ミチコ',
+      email: "0@0",
+      post_code: '0000000',
+      address: '東京都渋谷区神園町0-0',
+      phone_number: '00011112222',
+      is_active: true
+    },
+    {
+      password: "111111",
+      last_name: '難波',
+      first_name: '専太',
+      last_name_kana: "ナンバ",
+      first_name_kana: "センタ",
+      email: "1@1",
+      post_code: '1111111',
+      address: '東京都渋谷区神園町0-1',
+      phone_number: "00088889999",
+      is_active: true
+    },
+    {
+      password: "222222",
+      last_name: '六本木',
+      first_name: '一',
+      last_name_kana: 'ロッポンギ',
+      first_name_kana: 'ハジメ',
+      email: "2@2",
+      post_code: '2222222',
+      address: '東京都渋谷区神園町0-2',
+      phone_number: "09177772222",
+      is_active: false,
+      
+    },
+    {
+      password: "333333",
+      last_name: '尾形',
+      first_name: '隆弘',
+      last_name_kana: 'オガタタ',
+      first_name_kana: 'タカヒロ',
+      email: "3@3",
+      post_code: '3333333',
+      address: '東京都渋谷区神園町0-3',
+      phone_number: "09177773333",
+      is_active: true,
+    }
+  ]
 )
 
 Genre.create!([
@@ -28,3 +69,43 @@ Genre.create!([
     { name: "和菓子" },
     { name: "アイス" }
 ])
+
+Item.create!(
+  [
+    {
+      name: "ショートケーキ",
+      introduction: "美味しいケーキだよ",
+      price: "2900",
+      genre_id: 1,
+      is_active: "true",
+    },
+    {
+      name: "シュークリーム",
+      introduction: "６個あるよ",
+      price: "1650",
+      genre_id: 1,
+      is_active: "true",
+    },
+    {
+      name: "甘いプリン",
+      introduction: "プリンが一番おいしい",
+      price: "460",
+      genre_id: 3,
+      is_active: "true",
+    },
+    {
+      name: "９種のマカロン",
+      introduction: "マカロンが９種類！",
+      price: "2486",
+      genre_id: 2,
+      is_active: "true",
+    },
+    {
+      name: "チョコアイス３種類",
+      introduction: "チョコ、おいしい",
+      price: "2728",
+      genre_id: 5,
+      is_active: "true",
+    },
+  ]
+)
